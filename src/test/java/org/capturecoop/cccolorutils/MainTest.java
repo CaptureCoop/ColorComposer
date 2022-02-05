cfb050bad34029ecc173910f81e356c3614264f0
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-02-05 09:22:38 +0100

    Added look and feel to test

--- src/test/java/org/capturecoop/cccolorutils/MainTest.java
+++ src/test/java/org/capturecoop/cccolorutils/MainTest.java
@@ -2,10 +2,12 @@ package org.capturecoop.cccolorutils;
 
 import org.capturecoop.cccolorutils.chooser.CCColorChooser;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class MainTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         new CCColorChooser(new CCColor(Color.GREEN), "Cool chooser", 512, 512, true, null, null);
     }
 }
