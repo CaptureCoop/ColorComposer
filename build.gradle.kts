052ebcfb9e423305ce464451d17056833aa16556
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2026-01-31 12:50:37 +0100

    Add Maven Publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "2.3.0"
+    id("org.jetbrains.dokka") version "2.1.0"
+    id("com.vanniktech.maven.publish") version "0.36.0"
 }
 
 group = "org.capturecoop"
@@ -12,9 +12,53 @@ repositories {
 }
 
 dependencies {
-    implementation("org.capturecoop:CCUtils:1.9.6") //CaptureCoop Common Utils
+    implementation("org.capturecoop:defaultdepot:1.9.7")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    jvmToolchain(21)
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val dokkaHtml by tasks.getting(org.jetbrains.dokka.gradle.DokkaTask::class)
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates("org.capturecoop", "colorcomposer", version.toString())
+
+    pom {
+        name.set("Color Composer")
+        description.set("Color Utility Functions and Color Chooser")
+        inceptionYear.set("2022")
+        url.set("https://github.com/CaptureCoop/ColorComposer/")
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://github.com/CaptureCoop/ColorComposer/blob/main/LICENSE")
+                distribution.set("https://github.com/CaptureCoop/ColorComposer/blob/main/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("SvenWollinger")
+                name.set("Sven Wollinger")
+                url.set("https://wollinger.io")
+            }
+        }
+        scm {
+            url.set("https://github.com/CaptureCoop/ColorComposer/")
+            connection.set("scm:git:git://github.com/CaptureCoop/ColorComposer.git")
+            developerConnection.set("scm:git:ssh://git@github.com/CaptureCoop/ColorComposer.git")
+        }
+    }
 }
